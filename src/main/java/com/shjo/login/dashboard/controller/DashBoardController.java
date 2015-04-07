de84dd8af89ff94915cce62c0a5c63653a3a49b4
suheeJo <jo9172sh@gmail.com>
2015-04-07 16:01:53 +0900

    add base spring project

---
+++ src/main/java/com/shjo/login/dashboard/controller/DashBoardController.java
@@ -0,0 +1,30 @@
+package com.shjo.login.dashboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class DashBoardController {
+	
+	/**
+	 * <pre> 
+	 * 대시보드 페이지를 반환한다.
+	 * 
+	 * history
+	 * 2015. 4. 7. by 조수희
+	 * 초기 개발
+	 * </pre>
+	 *
+	 * @since 2015. 4. 7.오후 4:00:56
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/dashboard")
+	public String getDashBoardPage() throws Exception {
+		log.debug("#### getDashBoardPage()");
+		return "dashboard/index";
+	}
+}
---
+++ src/main/java/com/shjo/login/login/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.shjo.login.login.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class LoginController {
+
+	/**
+	 * <pre> 
+	 * 로그인 페이지를 반환한다.
+	 * 
+	 * history
+	 * 2015. 4. 7. by 조수희
+	 * 초기 개발
+	 * </pre>
+	 *
+	 * @since 2015. 4. 7.오후 4:00:11
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/")
+	public String getLoginPage() throws Exception {
+		log.debug("#### getLoginPage()");
+		
+		return "index";
+	}
+}
